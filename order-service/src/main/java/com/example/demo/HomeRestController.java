c914cd56e09b71549f0f6a6696b590a31d1c6c94
saurabhsharmaj <saurabh.find@gmail.com>
2023-11-22 19:29:55 +0530

    Add files via upload

---
+++ order-service/src/main/java/com/example/demo/HomeRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {	
+
+		@GetMapping("/")
+		public String getProcessDefinitions() {
+			return "OrderService-"+UUID.randomUUID().toString();
+		}
+		
+}

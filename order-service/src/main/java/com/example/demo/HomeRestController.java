38144c2331ceb0c464ad1a71b90c097fa266578d
saurabhsharmaj <saurabh.find@gmail.com>
2023-12-27 15:11:57 +0530

    Add files via upload

--- order-service/src/main/java/com/example/demo/HomeRestController.java
+++ order-service/src/main/java/com/example/demo/HomeRestController.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+
+
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeRestController {	
-
+	Logger log = LogManager.getLogger(HomeRestController.class);
+		
 		@GetMapping("/")
 		public String getProcessDefinitions() {
-			return "OrderService-"+UUID.randomUUID().toString();
+			UUID uuid=UUID.randomUUID();
+			log.info("OrderService-"+uuid.toString());
+			return "OrderService-"+uuid.toString();
 		}
 		
 }
